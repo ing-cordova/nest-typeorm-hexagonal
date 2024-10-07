106ed75eef26dbbf6556b9d2ac5b517f952e7bde
Andres Cordova <aecordova@innovacion.gob.sv>
2024-10-07 14:15:39 -0600

    add: documentation for delete endpoint

--- src/context/authUser/infraestructure/http-api/delete-authuser-by-id/delete-by-id.controller.ts
+++ src/context/authUser/infraestructure/http-api/delete-authuser-by-id/delete-by-id.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Delete, Param, ParseIntPipe } from "@nestjs/common";
 import { DeleteAuthUserByIdUseCase } from "src/context/authUser/application/delete-authuser-by-id-use-case/delete-authuser-by-id-use-case";
 import { DeleteByIdHttpDto } from "./delete-by-id-http-dto";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiParam, ApiTags } from "@nestjs/swagger";
 
 @ApiTags('authuser')
 @Controller('authuser')
@@ -9,6 +9,12 @@ export class DeleteAuthUserByIdController {
 
     constructor(private readonly deleteAuthUserByIdUseCase: DeleteAuthUserByIdUseCase) {}
 
+    @ApiParam({
+        name: 'id',
+        type: Number,
+        required: true,
+        description: 'The id of the authuser',
+      })
     @Delete('/delete/:id')
     async run(@Param('id', ParseIntPipe) id: number): Promise<void> {
         const params = new DeleteByIdHttpDto();
