7ad4930ba5fea179aa1d2ea54cbdd1c429284be7
Andres Cordova <aecordova@innovacion.gob.sv>
2024-10-08 13:45:04 -0600

    Add JwtAuthGuard for JWT authentication

---
+++ src/context/services/jwt.guard.ts
@@ -0,0 +1,5 @@
+import { AuthGuard } from "@nestjs/passport";
+import { Injectable } from "../shared/dependency-injection/injectable";
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') { }
