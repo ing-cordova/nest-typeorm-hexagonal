b5353f28e7100585d41cb44b6e429ea906b5f843
Andres Cordova <aecordova@innovacion.gob.sv>
2024-10-08 10:01:39 -0600

    update: password service

--- src/context/services/password-service.ts
+++ src/context/services/password-service.ts
@@ -1,7 +1,7 @@
 import * as bycript from 'bcrypt';
 
 export const encryptPassword = (password: string) => {
-    return bycript.hashSync(password, 256);
+    return bycript.hashSync(password, 10);
 };
 
 export const decryptPassword = (password: string, hash: string) => {
