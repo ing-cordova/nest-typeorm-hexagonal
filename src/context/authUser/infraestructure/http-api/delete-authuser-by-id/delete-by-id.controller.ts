3c8a6d9d099555e7569dba092916104feb14b695
Andres Cordova <aecordova@innovacion.gob.sv>
2024-10-16 11:02:51 -0600

    update: add authentication into delete endpoint

--- src/context/authUser/infraestructure/http-api/delete-authuser-by-id/delete-by-id.controller.ts
+++ src/context/authUser/infraestructure/http-api/delete-authuser-by-id/delete-by-id.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Delete, Param, ParseIntPipe } from "@nestjs/common";
+import { Controller, Delete, Param, ParseIntPipe, UseGuards } from "@nestjs/common";
 import { DeleteAuthUserByIdUseCase } from "src/context/authUser/application/delete-authuser-by-id-use-case/delete-authuser-by-id-use-case";
 import { DeleteByIdHttpDto } from "./delete-by-id-http-dto";
 import { ApiParam, ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "src/context/services/jwt.guard";
+import { GetInformationByToken } from "src/context/services/get-information.decorator";
 
 @ApiTags('authuser')
 @Controller('authuser')
@@ -9,14 +11,19 @@ export class DeleteAuthUserByIdController {
 
     constructor(private readonly deleteAuthUserByIdUseCase: DeleteAuthUserByIdUseCase) {}
 
+    /**
+     * GIBT: Get Information By Token
+     */
     @ApiParam({
         name: 'id',
         type: Number,
         required: true,
         description: 'The id of the authuser',
-      })
+    })
+    @UseGuards(JwtAuthGuard)
     @Delete('/delete/:id')
-    async run(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    async run(@Param('id', ParseIntPipe) id: number, @GetInformationByToken() gibt: any): Promise<void> {
+        console.log(gibt.username);
         const params = new DeleteByIdHttpDto();
         params.id = id;
         await this.deleteAuthUserByIdUseCase.execute(params);
