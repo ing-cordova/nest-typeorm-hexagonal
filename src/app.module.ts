e5d635d6635e9ae1f710e861001b7f255cef02e3
Andres Cordova <aecordova@innovacion.gob.sv>
2024-10-30 09:00:49 -0600

    update: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthUserModule } from './context/authUser/infraestructure/authuser.module';
+import { AuthUserModule } from './context/api/authUser/infraestructure/authuser.module';
+import { UserTypeModule } from './context/api/userType/infraestructure/user-type.module';
+import { CountryModule } from './context/api/country/infraestructure/country.module';
+import { CityModule } from './context/api/City/infraestructure/city.module';
 
 @Module({
   imports: [
@@ -21,11 +24,14 @@ import { AuthUserModule } from './context/authUser/infraestructure/authuser.modu
         database: configService.get<string>('DB_DATABASE'),
         autoLoadEntities: true,
         synchronize: true,  // Turn off in production mode
-        dropSchema: false,  // Turn off in production mode
+        dropSchema: true,  // Turn off in production mode
       }),
     }),
     
     AuthUserModule,
+    UserTypeModule,
+    CountryModule,
+    CityModule
   ],
 })
 export class AppModule {}
