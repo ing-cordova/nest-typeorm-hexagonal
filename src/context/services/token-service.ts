f8945e1d7dda2426e2d4af404223347c2592d4aa
Andres Cordova <aecordova@innovacion.gob.sv>
2024-11-04 13:16:39 -0600

    add: refresh token service

--- src/context/services/token-service.ts
+++ src/context/services/token-service.ts
@@ -1,6 +1,7 @@
 import * as jwt from 'jsonwebtoken';
 import { ConfigService } from '@nestjs/config';
 import { AuthUser } from '../api/authUser/domain/authuser.model';
+import exp from 'constants';
 
 const configService = new ConfigService();
 
@@ -16,10 +17,26 @@ export const generateToken = (payload: any) => {
     return token;
 };
 
+export const generateRefreshToken = (payload: any) => {
+    const refreshToken = jwt.sign(
+        payload,
+        configService.get<string>('REFRESH_TOKEN_SECRET'),
+        {
+            expiresIn: configService.get<string>('REFRESH_TOKEN_EXPIRATION'),
+        }
+    );
+
+    return refreshToken;
+};
+
 export const verifyToken = (token: string) => {
     return jwt.verify(token, configService.get<string>('TOKEN_SECRET'));
 };
 
 export const generateAppToken = (authUser: AuthUser) => {
     return generateToken({user_type: authUser.userType.name, username: authUser.username, location: authUser.country.name});
- }
+};
+
+export const generateAppRefreshToken = () => {
+    return generateRefreshToken({type: 'refresh'});
+}
