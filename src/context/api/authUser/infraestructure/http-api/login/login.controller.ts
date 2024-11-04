88e86facd982cb6749dce685bc240fb12ac56b94
Andres Cordova <aecordova@innovacion.gob.sv>
2024-11-04 10:17:21 -0600

    update: email field and message updated into login controller

--- src/context/api/authUser/infraestructure/http-api/login/login.controller.ts
+++ src/context/api/authUser/infraestructure/http-api/login/login.controller.ts
@@ -3,6 +3,7 @@ import { ApiTags } from "@nestjs/swagger";
 import { LoginUseCase } from "src/context/api/authUser/application/login-use-case/login-use-case";
 import { LoginHttpDto } from "./login-http.dto";
 import { generateAppToken } from "src/context/services/token-service";
+import { encryptPassword } from "src/context/services/password-service";
 
 @ApiTags('authuser')
 @Controller('authuser')
@@ -12,8 +13,8 @@ export class LoginController {
     @Post('login')
     @HttpCode(200)
     async login(@Body() body: LoginHttpDto) {
-        const userFound = await this.loginUseCase.execute(body.username, body.password);
-        if (!userFound) throw new HttpException('User not found', 404);
+        const userFound = await this.loginUseCase.execute(body.email, body.password);
+        if (!userFound) throw new HttpException("We're sorry, we can not find your account. :(", 404);
         return { token: generateAppToken(userFound) };
     }
 }
