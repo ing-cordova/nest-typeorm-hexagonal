bb293d0045da7d891a52c0ff748f914161ab318f
Andres Cordova <aecordova@innovacion.gob.sv>
2024-11-05 09:57:31 -0600

    update: jwt imports...

--- src/context/api/authUser/infraestructure/http-api/delete-authuser-by-id/delete-by-id.controller.ts
+++ src/context/api/authUser/infraestructure/http-api/delete-authuser-by-id/delete-by-id.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Delete, Param, ParseIntPipe, UseGuards } from "@nestjs/comm
 import { DeleteAuthUserByIdUseCase } from "src/context/api/authUser/application/delete-authuser-by-id-use-case/delete-authuser-by-id-use-case";
 import { DeleteByIdHttpDto } from "./delete-by-id-http-dto";
 import { ApiParam, ApiTags } from "@nestjs/swagger";
-import { JwtAuthGuard } from "src/context/services/jwt.guard";
+import { JwtAuthGuard } from "src/context/services/jwt/jwt.guard";
 import { GetInformationByToken } from "src/context/services/get-information.decorator";
 
 @ApiTags('authuser')
