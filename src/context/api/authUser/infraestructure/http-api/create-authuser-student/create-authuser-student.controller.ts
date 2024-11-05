1070b613ee2c818847ef31ebd20fd4a9ed156875
Andres Cordova <aecordova@innovacion.gob.sv>
2024-11-05 09:55:02 -0600

    add: controller logic for authuser type student

---
+++ src/context/api/authUser/infraestructure/http-api/create-authuser-student/create-authuser-student.controller.ts
@@ -0,0 +1,94 @@
+import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import { AuthUser } from 'src/context/api/authUser/domain/authuser.model';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateAuthUserStudentUseCase } from '../../../application/create-authuser-student-use-case/create-authuser-use-case';
+import { CreateAuthUserStudentHttpDto } from './create-authuser-student-http-dto';
+import { TypeOrmAuthUserRepository } from '../../repositories/typeorm-authuser.repository';
+
+@ApiTags('authuser')
+@Controller('authuser')
+export class CreateAuthUserStudentController {
+    constructor(
+        private createAuthUserStudentUseCase: CreateAuthUserStudentUseCase,
+        private readonly typeOrmAuthUserRepository: TypeOrmAuthUserRepository
+    ) { }
+
+    @Post('/student-enrollment')
+    @ApiBody({
+        description: 'Atributes requerid to create a new user at the system',
+        type: CreateAuthUserStudentUseCase,
+    })
+    @ApiResponse({
+        status: 201,
+        description: 'The user with student role has been successfully created.',
+        schema: {
+            type: 'object',
+            properties: {
+                authUser: {
+                    type: 'object',
+                    properties: {
+                        id: { type: 'integer', example: 16 },
+                        user_type_id: { type: 'integer', example: 3 },
+                        first_name: { type: 'string', example: 'Andres' },
+                        last_name: { type: 'string', example: 'Cordova' },
+                        phone_number: { type: 'string', example: '90764567' },
+                        email: { type: 'string', example: 'andres.cordova6@yopmail.com' },
+                        username: { type: 'string', example: 'acordova7' },
+                        country_id: { type: 'integer', example: 66 },
+                        state_id: { type: 'integer', example: 1109 },
+                        address: { type: 'string', example: 'La Palma, Chalatenango' },
+                        accepted_terms: { type: 'boolean', example: true },
+                        created_at: { type: 'string', format: 'date-time', example: '2024-11-05T15:17:41.081Z' },
+                        second_name: { type: 'string', nullable: true, example: null },
+                        second_last_name: { type: 'string', nullable: true, example: null },
+                        email_verified_at: { type: 'string', format: 'date-time', nullable: true, example: null },
+                        updated_at: { type: 'string', format: 'date-time', nullable: true, example: null },
+                        deleted_at: { type: 'string', format: 'date-time', nullable: true, example: null },
+                        is_temporal_password: { type: 'boolean', example: true }
+                    },
+                },
+            },
+        },
+    })
+    async run(
+        @Body() createAuthUserStudentHttpDto: CreateAuthUserStudentHttpDto
+    ): Promise<{ authUser: AuthUser }> {
+        try {
+            return await this.createAuthUserStudentUseCase.execute({
+                first_name: createAuthUserStudentHttpDto.first_name,
+                last_name: createAuthUserStudentHttpDto.last_name,
+                phone_number: createAuthUserStudentHttpDto.phone_number,
+                email: createAuthUserStudentHttpDto.email,
+                country_id: createAuthUserStudentHttpDto.country_id,
+                state_id: createAuthUserStudentHttpDto.state_id,
+                address: createAuthUserStudentHttpDto.address,
+                accepted_terms: createAuthUserStudentHttpDto.accepted_terms,
+                username: await this.generateUsername(createAuthUserStudentHttpDto.first_name, createAuthUserStudentHttpDto.last_name)
+            });
+        }
+        catch (error) {
+            throw new HttpException(error.message, error.status);
+        }
+    }
+
+    private generateUsername = async (firstName: string, lastName: string): Promise<string> => {
+        try {
+            let username = `${firstName[0].toLowerCase()}${lastName.toLowerCase()}`;
+            let userExists = await this.typeOrmAuthUserRepository.findByUsername(username);
+
+            while (userExists) {
+                const randomSuffix = Math.random() < 0.5
+                    ? String.fromCharCode(97 + Math.floor(Math.random() * 26))
+                    : Math.floor(Math.random() * 10).toString();
+
+                username = `${firstName[0].toLowerCase()}${lastName.toLowerCase()}${randomSuffix}`;
+                userExists = await this.typeOrmAuthUserRepository.findByUsername(username);
+            }
+
+            return username;
+        }
+        catch (error) {
+            throw new HttpException('Error while generating username...', error.status);
+        }
+    };
+}
