8693a1bfb1d52bb1e771eb891c784d4ffc8665ea
Andres Cordova <aecordova@innovacion.gob.sv>
2024-11-11 13:33:56 -0600

    add: JwtRefreshTokenStrategy

---
+++ src/context/services/jwt/jwt-refresh-token.strategy.ts
@@ -0,0 +1,20 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { Injectable } from "../../shared/dependency-injection/injectable";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { ConfigService } from '@nestjs/config';
+
+
+const config = new ConfigService();
+
+@Injectable()
+export class JwtRefreshTokenStrategy extends PassportStrategy(Strategy) {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: config.get<string>('REFRESH_TOKEN_SECRET'),
+        });
+    }
+
+    async validate(payload: any) { return payload; }
+}
