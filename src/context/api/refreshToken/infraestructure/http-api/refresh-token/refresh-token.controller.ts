4400cc28ff7e7b9baf9733d91251e206860f2488
Andres Cordova <aecordova@innovacion.gob.sv>
2024-11-12 09:13:50 -0600

    fix: refresh token endpoint guard using

--- src/context/api/refreshToken/infraestructure/http-api/refresh-token/refresh-token.controller.ts
+++ src/context/api/refreshToken/infraestructure/http-api/refresh-token/refresh-token.controller.ts
@@ -1,9 +1,9 @@
 import { ClassSerializerInterceptor, Controller, HttpException, Post, UseGuards, UseInterceptors } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GetInformationByToken } from 'src/context/services/get-information.decorator';
-import { JwtAuthGuard } from 'src/context/services/jwt/jwt.guard';
 import { generateAPPTokenAndRefreshToken } from 'src/context/services/token-service';
 import { FindAuthUserByUsernameUseCase } from 'src/context/api/authUser/application/find-authuser-by-username-use-case/find-authuser-by-username-use-case';
+import { JwtAuthRefreshGuard } from 'src/context/services/jwt/jwt-refresh.guard';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -12,7 +12,7 @@ export class RefreshTokenController {
     constructor(private readonly findAuthuserByUsernameUseCase: FindAuthUserByUsernameUseCase,) { }
 
     @Post('/refresh-token')
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthRefreshGuard)
     @ApiResponse({
         status: 200,
         description: 'Refresh token created successfully',
