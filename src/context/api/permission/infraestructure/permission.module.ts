188c6af42a5c6c1edfa21fe6a71fc589fdcb847f
Andres Cordova <aecordova@innovacion.gob.sv>
2024-11-14 14:12:32 -0600

    update: permission module

--- src/context/api/permission/infraestructure/permission.module.ts
+++ src/context/api/permission/infraestructure/permission.module.ts
@@ -3,16 +3,20 @@ import { ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { JwtModule } from "@nestjs/jwt";
 import { Permission } from "../domain/permission.model";
+import { PermissionService } from "src/context/services/permissions.service";
+import { PermissionUserType } from "../../permissionUserType/domain/permission-user-type.model";
 
 const config = new ConfigService();
 @Module({
     imports : [
-        TypeOrmModule.forFeature([Permission]),
+        TypeOrmModule.forFeature([Permission, PermissionUserType]),
         JwtModule.register({
             secret: config.get<string>('TOKEN_SECRET'),
             signOptions: { expiresIn: config.get<string>('TOKEN_EXPIRATION') },
         })
-    ] 
+    ],
+    providers: [PermissionService],
+    exports: [PermissionService]
 })
 
 export class PermissionModule {}
