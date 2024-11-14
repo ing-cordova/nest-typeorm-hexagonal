25dcccf6d14f8c57e3401ad0941cb872cf1fa05d
Andres Cordova <aecordova@innovacion.gob.sv>
2024-11-14 14:13:58 -0600

    add: method for getting usertype enum string

--- src/context/api/userType/domain/user-type.enum.ts
+++ src/context/api/userType/domain/user-type.enum.ts
@@ -4,3 +4,14 @@ export enum UserTypeEnum {
     STUDENT = 3,
     TEACHER = 4
 }
+
+const userTypeMap: { [key: string]: number } = {
+    'SuperAdmin': UserTypeEnum.SUPER_ADMINISTRATOR,
+    'Administrator': UserTypeEnum.ADMINISTRATOR,
+    'Student': UserTypeEnum.STUDENT,
+    'Teacher': UserTypeEnum.TEACHER
+};
+
+export function getUserTypeId(userType: string): number | undefined {
+    return userTypeMap[userType];
+}
