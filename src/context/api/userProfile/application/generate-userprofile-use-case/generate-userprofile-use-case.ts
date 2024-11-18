386a828cdcd2fa2ee1a481ba14d388e807ab61d0
Andres Cordova <aecordova@innovacion.gob.sv>
2024-11-18 11:00:14 -0600

    update: use case now accepting user type id

--- src/context/api/userProfile/application/generate-userprofile-use-case/generate-userprofile-use-case.ts
+++ src/context/api/userProfile/application/generate-userprofile-use-case/generate-userprofile-use-case.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '../../../../shared/dependency-injection/injectable';
-import { UserTypeEnum } from '../../../userType/domain/user-type.enum';
 import { encryptPassword } from '../../../../services/password-service';
 import { HttpException } from '@nestjs/common';
 import { UserProfileRepository } from '../../domain/userprofile.repository';
 import { UserProfile } from '../../domain/userprofile.model';
 import { GenerateUserProfileUseCaseDto } from './generate-userprofile-use-case.dto';
+import { userTypeReverseMap } from 'src/context/api/userType/domain/user-type.enum';
 
 @Injectable()
 export class GenerateUserProfileUseCase {
@@ -18,7 +18,7 @@ export class GenerateUserProfileUseCase {
             const userProfile = new UserProfile();
             const passwordGenerated = this.generatePassword(8);
 
-            userProfile.user_type_id = UserTypeEnum.STUDENT;
+            userProfile.user_type_id = dto.user_type_id;
             userProfile.first_name = dto.first_name;
             userProfile.last_name = dto.last_name;
             userProfile.phone_number = dto.phone_number;
@@ -32,7 +32,7 @@ export class GenerateUserProfileUseCase {
 
             await this.userProfileRepository.create(userProfile);
             // TODO: Send email with the credentials.
-            console.log(`Dear ${userProfile.first_name} ${userProfile.last_name}, your credentials are the following:\nEmail: ${userProfile.email}\nPassword: [${passwordGenerated}]`);
+            console.log(`Welcome ${userTypeReverseMap[dto.user_type_id]}\nDear ${userProfile.first_name} ${userProfile.last_name}, your credentials are the following:\nEmail: ${userProfile.email}\nPassword: [${passwordGenerated}]`);
 
             return { userProfile };
         }
