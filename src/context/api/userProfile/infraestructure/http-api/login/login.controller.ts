c5d0198cc24762dc2ec6d6b3bd9b376f6358450d
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-02 11:05:34 -0600

    feat: using permission decorator into login endpoint

--- src/context/api/userProfile/infraestructure/http-api/login/login.controller.ts
+++ src/context/api/userProfile/infraestructure/http-api/login/login.controller.ts
@@ -1,9 +1,12 @@
-import { Body, ClassSerializerInterceptor, Controller, HttpCode, HttpException, Post, UseInterceptors } from "@nestjs/common";
+import { Body, ClassSerializerInterceptor, Controller, HttpCode, HttpException, Post, UseGuards, UseInterceptors } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { LoginHttpDto } from "./login-http.dto";
 import { generateAPPTokenAndRefreshToken } from "src/context/services/token-service";
 import { LoginUseCase } from "../../../application/login-use-case/login-use-case";
 import { PublicEndpoints } from "src/context/routes/routing";
+import { Permissions } from "src/context/decorators/permissions.decorator";
+import { PermissionEnum } from "src/context/api/permission/domain/permission.enum";
+import { PermissionsGuard } from "src/context/guards/permissions.guard";
 
 @ApiTags('public')
 @Controller()
@@ -12,6 +15,7 @@ export class LoginController {
     constructor(private readonly loginUseCase: LoginUseCase) { }
 
     @Post(PublicEndpoints.LOGIN)
+    @Permissions(PermissionEnum.LOGIN)
     @HttpCode(200)
     async login(@Body() body: LoginHttpDto) {
         const userFound = await this.loginUseCase.execute(body.email, body.password);
