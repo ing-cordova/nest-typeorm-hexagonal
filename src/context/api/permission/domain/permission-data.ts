526f3f8016f4b10a775e7ac0b4fcea7fb8a9ccd9
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-02 11:03:04 -0600

    update: information for seeding database

--- src/context/api/permission/domain/permission-data.ts
+++ src/context/api/permission/domain/permission-data.ts
@@ -1,16 +1,62 @@
+import { UserTypeEnum } from "../../userType/domain/user-type.enum";
 import { PermissionEnum } from "./permission.enum";
+
+const allUserTypes = Object.values(UserTypeEnum).filter(value => typeof value === 'number') as UserTypeEnum[];
+
+function expandUserTypes(userTypes: (UserTypeEnum | '*')[]): UserTypeEnum[] {
+    if (userTypes.includes('*')) {
+        return allUserTypes;
+    }
+    return userTypes as UserTypeEnum[];
+}
 
 export const PermissionsMainData = [
     {
         id: 1,
-        can: PermissionEnum.GENERATE_PROFILE,
-        description: "Permission to add new user into the system",
-        user_types: [1, 2]
+        can: PermissionEnum.LOGIN,
+        description: "Permission to login into the system",
+        user_types: expandUserTypes(['*'])
     },
     {
         id: 2,
+        can: PermissionEnum.GENERATE_PROFILE,
+        description: "Permission to add new user into the system",
+        user_types: expandUserTypes([UserTypeEnum.SUPER_ADMINISTRATOR, UserTypeEnum.ADMINISTRATOR])
+    },
+    {
+        id: 3,
         can: PermissionEnum.CHANGE_TEMPORAL_PASSWORD,
         description: "Permission to change the temporal password",
-        user_types: [1, 2, 3, 4]
+        user_types: expandUserTypes(['*'])
     },
+    {
+        id: 4,
+        can: PermissionEnum.CREATE_PROFILE,
+        description: "Permission to create a new user",
+        user_types: expandUserTypes([UserTypeEnum.STUDENT])
+    },
+    {
+        id: 5,
+        can: PermissionEnum.UPDATE_PROFILE,
+        description: "Permission to update the user information",
+        user_types: expandUserTypes(['*'])
+    },
+    {
+        id: 6,
+        can: PermissionEnum.DELETE_PROFILE,
+        description: "Permission to delete an user of the system",
+        user_types: expandUserTypes([UserTypeEnum.SUPER_ADMINISTRATOR, UserTypeEnum.ADMINISTRATOR])
+    },
+    {
+        id: 7,
+        can: PermissionEnum.VIEW_PROFILE,
+        description: "Permission to view the user information",
+        user_types: expandUserTypes(['*'])
+    },
+    {
+        id: 8,
+        can: PermissionEnum.VIEW_ALL_PROFILE,
+        description: "Permission to view all the users information",
+        user_types: expandUserTypes([UserTypeEnum.SUPER_ADMINISTRATOR, UserTypeEnum.ADMINISTRATOR])
+    }
 ];
