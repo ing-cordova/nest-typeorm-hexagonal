903f8b717875977996f4a76bac095a872e168a41
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-02 09:21:24 -0600

    update: permissions

--- src/context/api/permission/domain/permission-data.ts
+++ src/context/api/permission/domain/permission-data.ts
@@ -1,3 +1,4 @@
+import { UserTypeEnum } from "../../userType/domain/user-type.enum";
 import { PermissionEnum } from "./permission.enum";
 
 export const PermissionsMainData = [
@@ -5,12 +6,12 @@ export const PermissionsMainData = [
         id: 1,
         can: PermissionEnum.GENERATE_PROFILE,
         description: "Permission to add new user into the system",
-        user_types: [1, 2]
+        user_types: [UserTypeEnum.SUPER_ADMINISTRATOR, UserTypeEnum.ADMINISTRATOR]
     },
     {
         id: 2,
         can: PermissionEnum.CHANGE_TEMPORAL_PASSWORD,
         description: "Permission to change the temporal password",
-        user_types: [1, 2, 3, 4]
+        user_types: [UserTypeEnum.SUPER_ADMINISTRATOR, UserTypeEnum.ADMINISTRATOR, UserTypeEnum.DOCTOR, UserTypeEnum.ENFEREMERO, UserTypeEnum.CAJERO]
     },
 ];
