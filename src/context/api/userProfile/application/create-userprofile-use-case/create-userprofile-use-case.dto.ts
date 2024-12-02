6ace76797d6e832df727eb7e78205ae6d32d12c8
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-02 14:49:09 -0600

    update: create user profile dto

--- src/context/api/userProfile/application/create-userprofile-use-case/create-userprofile-use-case.dto.ts
+++ src/context/api/userProfile/application/create-userprofile-use-case/create-userprofile-use-case.dto.ts
@@ -1,5 +1,9 @@
 export interface CreateUserProfileUseCaseDto {
+  user_type_id: number;
+  first_name: string;
+  last_name: string;
   username: string;
   email: string;
   password: string;
+  country_id: number;
 }
