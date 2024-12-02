0f8f59af2bcd4ed3e93d0bc79b8b5998559de203
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-02 09:22:33 -0600

    update: user type enum

--- src/context/api/userType/domain/user-type.enum.ts
+++ src/context/api/userType/domain/user-type.enum.ts
@@ -1,22 +1,25 @@
 export enum UserTypeEnum {
     SUPER_ADMINISTRATOR = 1,
     ADMINISTRATOR = 2,
-    STUDENT = 3,
-    TEACHER = 4
+    DOCTOR = 3,
+    ENFEREMERO = 4,
+    CAJERO = 5,
 }
 
 const userTypeMap: { [key: string]: number } = {
     'SuperAdmin': UserTypeEnum.SUPER_ADMINISTRATOR,
     'Administrator': UserTypeEnum.ADMINISTRATOR,
-    'Student': UserTypeEnum.STUDENT,
-    'Teacher': UserTypeEnum.TEACHER
+    'Doctor': UserTypeEnum.DOCTOR,
+    'Enfermero': UserTypeEnum.ENFEREMERO,
+    'Cajero': UserTypeEnum.CAJERO
 };
 
 export const userTypeReverseMap: { [key: number]: string } = {
     [UserTypeEnum.SUPER_ADMINISTRATOR]: 'SuperAdmin',
     [UserTypeEnum.ADMINISTRATOR]: 'Administrator',
-    [UserTypeEnum.STUDENT]: 'Student',
-    [UserTypeEnum.TEACHER]: 'Teacher'
+    [UserTypeEnum.DOCTOR]: 'Doctor/a',
+    [UserTypeEnum.ENFEREMERO]: 'Enfermero/a',
+    [UserTypeEnum.CAJERO]: 'Cajero/a'
 };
 
 export function getUserTypeId(userType: string): number | undefined {
