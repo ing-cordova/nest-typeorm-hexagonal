1a310883710338069175658f5130489af46a2e11
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-03 15:02:08 -0600

    update: delete by id endpoint

--- src/context/api/userProfile/infraestructure/http-api/delete-userprofile-by-id/delete-by-id.controller.ts
+++ src/context/api/userProfile/infraestructure/http-api/delete-userprofile-by-id/delete-by-id.controller.ts
@@ -1,16 +1,20 @@
-import { ClassSerializerInterceptor, Controller, Delete, Param, ParseIntPipe, UseGuards, UseInterceptors } from "@nestjs/common";
+import { BadRequestException, ClassSerializerInterceptor, Controller, Delete, Param, ParseIntPipe, UseGuards, UseInterceptors } from "@nestjs/common";
 import { DeleteByIdHttpDto } from "./delete-by-id-http-dto";
 import { ApiParam, ApiTags } from "@nestjs/swagger";
 import { GetInformationByToken } from "src/context/services/get-information.decorator";
 import { DeleteUserProfileByIdUseCase } from "../../../application/delete-userprofile-by-id-use-case/delete-userprofile-by-id-use-case";
 import { JwtAuthGuard } from "src/context/guards/jwt.guard";
+import { PermissionsGuard } from "src/context/guards/permissions.guard";
+import { Permissions } from "src/context/decorators/permissions.decorator";
+import { PermissionEnum } from "src/context/api/permission/domain/permission.enum";
+import { PrivateEndpoints } from "src/context/routes/routing";
 
-@ApiTags('user-profile')
-@Controller('user-profile')
+@ApiTags('private')
+@Controller()
 @UseInterceptors(ClassSerializerInterceptor)
 export class DeleteUserProfileByIdController {
 
-    constructor(private readonly deleteUserProfileByIdUseCase: DeleteUserProfileByIdUseCase) {}
+    constructor(private readonly deleteUserProfileByIdUseCase: DeleteUserProfileByIdUseCase) { }
 
     /**
      * GIBT: Get Information By Token
@@ -21,12 +25,17 @@ export class DeleteUserProfileByIdController {
         required: true,
         description: 'The id of the userProfile',
     })
-    @UseGuards(JwtAuthGuard)
-    @Delete('/delete/:id')
+    @UseGuards(JwtAuthGuard, PermissionsGuard)
+    @Permissions(PermissionEnum.DELETE_PROFILE)
+    @Delete(PrivateEndpoints.DELETE_PROFILE)
     async run(@Param('id', ParseIntPipe) id: number, @GetInformationByToken() gibt: any): Promise<void> {
-        console.log(gibt.username);
-        const params = new DeleteByIdHttpDto();
-        params.id = id;
-        await this.deleteUserProfileByIdUseCase.execute(params);
+        try {
+            const params = new DeleteByIdHttpDto();
+            params.id = id;
+            await this.deleteUserProfileByIdUseCase.execute(params);
+        }
+        catch (error) {
+            throw new BadRequestException(error.message);
+        }
     }
 }
