f59c470ca9768874dd82203e1800165b8a0dba55
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-03 10:07:34 -0600

    update: userprofile use case login

--- src/context/api/userProfile/application/update-userprofile-use-case/update-userprofile-use-case.ts
+++ src/context/api/userProfile/application/update-userprofile-use-case/update-userprofile-use-case.ts
@@ -1,23 +1,23 @@
 import * as bycript from 'bcrypt';
 import { Injectable } from "../../../../shared/dependency-injection/injectable";
 import { UserProfileRepository } from '../../domain/userprofile.repository';
-import { UpdateUserProfileUseCaseParamsDto } from './update-userprofile-use-case-params.dto';
-import { UpdateUserProfileUseCaseDto } from './update-userprofile-use-case.dto';
 import { UserProfile } from '../../domain/userprofile.model';
 import { UserProfileCannotUpdateException } from '../../domain/userprofile-cannot-update-exception';
-
+import { UpdateUserProfileUseCaseDto } from './update-userprofile-use-case.dto';
 
 @Injectable()
 export class UpdateUserProfileUseCase {
     constructor(private readonly userProfileRepository: UserProfileRepository) { }
 
-    async execute(params: UpdateUserProfileUseCaseParamsDto, reqBody: UpdateUserProfileUseCaseDto): Promise<{ userProfile: UserProfile }> {
+    async execute(username: string, reqBody: UpdateUserProfileUseCaseDto): Promise<{ userProfile: UserProfile }> {
         try {
-            const userProfile = await this.userProfileRepository.updateById(params.id, reqBody);
+            const { id } = await this.userProfileRepository.findByUsername(username);
+            const { user_type_id, ...rest } = reqBody;
+            const userProfile = await this.userProfileRepository.updateById(id, rest);
             return { userProfile };
         }
         catch (error) {
-            throw new UserProfileCannotUpdateException(params.id);
+            throw new UserProfileCannotUpdateException(reqBody.id);
         }
     }
-}
+}   
