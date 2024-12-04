b16eed458587b8e72879b5702ed15e3fffe55f86
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-04 13:30:22 -0600

    add: new pagination service

---
+++ src/context/services/pagination/pagination.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "src/context/shared/dependency-injection/injectable";
+
+@Injectable()
+export class PaginationService {
+    paginate<T>(data: T[], total: number, page: number, limit: number): { data: T[], total: number, nextPage: number | null, prevPage: number | null, limit: number } {
+        const nextPage = (page * limit) < total ? page + 1 : null;
+        const prevPage = page > 1 ? page - 1 : null;
+        return { data, total, nextPage, prevPage, limit };
+    }
+}
