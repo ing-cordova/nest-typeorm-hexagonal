c518094159b79f600084ce169b30d10f76c9ef8f
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-05 11:43:11 -0600

    update: documentation into get states by country id endpoint

--- src/context/api/state/infraestructure/http-api/find-by-country-id-http/find-by-country-id.controller.ts
+++ src/context/api/state/infraestructure/http-api/find-by-country-id-http/find-by-country-id.controller.ts
@@ -1,5 +1,5 @@
 import { ClassSerializerInterceptor, Controller, Get, Param, UseInterceptors } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 import { PrefixEndpointType, PrivateEndpoints, PublicEndpoints } from "src/context/routes/routing";
 import { FindByCountryIdUseCase } from "../../../application/find-by-country-id-use-case/find-by-country-id-use-case";
 
@@ -10,6 +10,22 @@ export class FindByCountryIdController {
     constructor(private readonly findByCountryIdUseCase: FindByCountryIdUseCase) { }
 
     @Get(PublicEndpoints.GET_STATES_BY_COUNTRY)
+    @ApiResponse({
+        status: 200,
+        description: 'Get all states filtered by country ID',
+        schema: {
+            type: 'array',
+            items: {
+                type: 'object',
+                properties: {
+                    id: { type: 'integer', example: 1107 },
+                    name: { type: 'string', example: 'Ahuachapán' },
+                    state_code: { type: 'string', example: 'AH' },
+                    country_id: { type: 'integer', example: 66 },
+                },
+            },
+        },
+    })
     async execute(@Param('countryId') countryId: number) {
         return this.findByCountryIdUseCase.execute(countryId);
     }
