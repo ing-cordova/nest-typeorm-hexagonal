2b565d281d8a8a3e76172c853eaed65f467cefc7
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-05 11:43:33 -0600

    update: documentation into get all countries endpoint

--- src/context/api/country/infraestructure/http-api/get-all-countries-http/get-all-countries.controller.ts
+++ src/context/api/country/infraestructure/http-api/get-all-countries-http/get-all-countries.controller.ts
@@ -1,5 +1,5 @@
 import { ClassSerializerInterceptor, Controller, Get, UseInterceptors } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 import { PrefixEndpointType, PublicEndpoints } from "src/context/routes/routing";
 import { GetCountriesUseCase } from "../../../application/get-countries-use-case/get-countries-use-case";
 
@@ -10,6 +10,26 @@ export class GetAllCountriesController {
     constructor(private getAllCountriesUseCase: GetCountriesUseCase) { }
 
     @Get(PublicEndpoints.GET_ALL_COUNTRIES)
+    @ApiResponse({
+        status: 200,
+        description: 'Get all countries',
+        schema: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              id: { type: 'integer', example: 1 },
+              name: { type: 'string', example: 'Afghanistan' },
+              iso2: { type: 'string', example: 'AF' },
+              iso3: { type: 'string', example: 'AFG' },
+              phone_code: { type: 'string', example: '93' },
+              region: { type: 'string', example: 'Asia' },
+              currency: { type: 'string', example: 'AFN' },
+            },
+          },
+        },
+      })
+      
     async getAllCountries() {
         return await this.getAllCountriesUseCase.execute();
     }
