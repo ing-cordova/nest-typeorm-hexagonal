bd499ab837d46f7c2efc3bd16b603782eb56b2ed
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-06 14:15:41 -0600

    add: create new message http controller

---
+++ src/context/api/messages/infraestructure/http-api/create-new-message-http.controller.ts
@@ -0,0 +1,24 @@
+import { BadRequestException, Body, ClassSerializerInterceptor, Controller, Post, UseInterceptors } from "@nestjs/common";
+import { ApiBody, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { PrefixEndpointType, PublicEndpoints } from "src/context/routes/routing";
+import { CreateNewMessageUseCase } from "../../application/create-new-message-use-case/create-new-message-use-case";
+import { CreateNewMessageHttpDto } from "./create-new-message-http-dto";
+
+@ApiTags(PrefixEndpointType.PUBLIC)
+@Controller()
+@UseInterceptors(ClassSerializerInterceptor)
+export class CreateNewMessageHttpController {
+    constructor(private readonly createNewMessageUseCase: CreateNewMessageUseCase) { }
+
+    @Post(PublicEndpoints.CREATE_NEW_MESSAGE)
+    @ApiBody({ type: CreateNewMessageHttpDto })
+    @ApiResponse({ status: 201, type: null })
+    async create(@Body() createNewMessageHttpDto: CreateNewMessageHttpDto): Promise<void> {
+        try {
+            return await this.createNewMessageUseCase.execute(createNewMessageHttpDto);
+        }
+        catch (error) {
+            throw new BadRequestException(error.message);
+        }
+    }
+}
