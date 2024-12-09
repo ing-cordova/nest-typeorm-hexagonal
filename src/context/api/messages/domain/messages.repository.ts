abc635691ee451edd1a0822f5cf69a68475c7f88
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-09 10:40:16 -0600

    add: paginated method into repository

--- src/context/api/messages/domain/messages.repository.ts
+++ src/context/api/messages/domain/messages.repository.ts
@@ -2,4 +2,6 @@ import { Messages } from "./messages.model";
 
 export abstract class MessagesRepository {
     abstract create(messages: Messages): Promise<void>;
+    abstract findAll(): Promise<Messages[]>;
+    abstract findAllWithPagination(page: number, limit: number): Promise<{ data: Messages[], total: number, nextPage: number | null, prevPage: number | null, limit: number }>;
 }
