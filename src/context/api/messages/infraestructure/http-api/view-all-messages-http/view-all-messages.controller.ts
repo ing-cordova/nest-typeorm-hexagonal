d6d7b186d4c9fefbd1092694393151d1742897bc
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-09 10:40:45 -0600

    add: view all messages controller

---
+++ src/context/api/messages/infraestructure/http-api/view-all-messages-http/view-all-messages.controller.ts
@@ -0,0 +1,28 @@
+import { BadRequestException, ClassSerializerInterceptor, Controller, DefaultValuePipe, Get, ParseIntPipe, Query, UseGuards, UseInterceptors } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { PrefixEndpointType, PrivateEndpoints } from "src/context/routes/routing";
+import { ViewAllMessagesUseCase } from "../../../application/view-all-messages-use-case/view-all-messages-use-case";
+import { JwtAuthGuard } from "src/context/guards/jwt.guard";
+import { PermissionsGuard } from "src/context/guards/permissions.guard";
+import { Permissions } from "../../../../../decorators/permissions.decorator";
+import { PermissionEnum } from "src/context/api/permission/domain/permission.enum";
+
+@ApiTags(PrefixEndpointType.PRIVATE)
+@Controller()
+@UseInterceptors(ClassSerializerInterceptor)
+export class ViewAllMessagesHttpController {
+    constructor(private readonly viewAllMessagesUseCase: ViewAllMessagesUseCase) { }
+
+    @Get(PrivateEndpoints.VIEW_ALL_MESSAGES)
+    @UseGuards(JwtAuthGuard, PermissionsGuard)
+    @Permissions(PermissionEnum.VIEW_ALL_MESSAGES)
+    async viewAllMessages(@Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+        @Query('limit', new DefaultValuePipe(5), ParseIntPipe) limit: number) {
+        try {
+            return await this.viewAllMessagesUseCase.execute(page, limit);
+        }
+        catch (error) {
+            throw new BadRequestException(error.message);
+        }
+    }
+}
