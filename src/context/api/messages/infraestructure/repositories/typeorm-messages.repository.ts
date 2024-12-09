2d3abafa1011a417f8d51f04347c7e699b97e17f
Andres Cordova <aecordova@innovacion.gob.sv>
2024-12-09 10:40:28 -0600

    update: typeorm repository

--- src/context/api/messages/infraestructure/repositories/typeorm-messages.repository.ts
+++ src/context/api/messages/infraestructure/repositories/typeorm-messages.repository.ts
@@ -16,4 +16,19 @@ export class TypeOrmMessagesRepository extends MessagesRepository {
     async create(message: Messages): Promise<void> {
         await this.repository.save(message);
     }
+
+    async findAll(): Promise<Messages[]> {
+        return await this.repository.find();
+    }
+
+    async findAllWithPagination(page: number, limit: number): Promise<{ data: Messages[], total: number, nextPage: number | null, prevPage: number | null, limit: number }> {
+        const [messages, total] = await this.repository.findAndCount({
+            skip: (page - 1) * limit,
+            take: limit,
+        });
+
+        const nextPage = (page * limit) < total ? page + 1 : null;
+        const prevPage = page > 1 ? page - 1 : null;
+        return { data: messages, total, nextPage, prevPage, limit };
+    }
 }
