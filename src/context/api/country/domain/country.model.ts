6c5d167156afbdd48de8617b1256d2b349df6278
Andres Cordova <aecordova@innovacion.gob.sv>
2025-01-15 08:59:26 -0600

    update: country model to include subregion

--- src/context/api/country/domain/country.model.ts
+++ src/context/api/country/domain/country.model.ts
@@ -1,6 +1,7 @@
 import { Exclude } from "class-transformer";
 import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Region } from "../../region/domain/region.model";
+import { SubRegion } from "../../subRegion/domain/sub-region.model";
 
 @Entity('country')
 export class Country {
@@ -22,8 +23,8 @@ export class Country {
     @Column({nullable: true})
     region_id: number;
 
-    @Column()
-    sub_region: string
+    @Column({nullable: true})
+    sub_region_id: number;
 
     @Column()
     currency: string;
@@ -52,4 +53,8 @@ export class Country {
     @ManyToOne(() => Region, region => region.id)
     @JoinColumn({ name: 'region_id' })
     region: Region;
+
+    @ManyToOne(() => SubRegion, sub_region => sub_region.id)
+    @JoinColumn({ name: 'sub_region_id' })
+    sub_region: SubRegion;
 }
